eba732dae745fc3981e354478450575c3659d1e8
pichaya <pichom95@gmail.com>
2018-08-20 22:12:59 +0700

    add register page and register service

---
+++ client/src/components/Register.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <h1> Register </h1>
+    <div> Email</div>
+    <input
+      type="email"
+      v-model="email"
+      name="email" />
+    <div> Password</div>
+    <input
+      type="password"
+      v-model="password"
+      name="password" />
+    <br>
+    <button @click="register"> Register </button>
+  </div>
+</template>
+
+<script>
+import AuthenService from '@/services/AuthenService'
+export default {
+  data () {
+    return {
+      email: '',
+      password: ''
+    }
+  },
+  methods: {
+    async register () {
+      const response = await AuthenService.register({
+        email: this.email,
+        password: this.password
+      })
+      console.log('Register button was clicked', response.data)
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
